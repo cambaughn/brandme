89536d852ae2522b34687be7941c3efa69e1209e
Cameron Baughn <cameron@ohsosuper.com>
2016-01-02 17:52:06 -0800

    tried to fix the flash message

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,8 +8,8 @@ class ContactsController < ApplicationController
 		@contact = Contact.new(params[:contact])
 	    @contact.request = request
 	    if @contact.deliver
-	      flash.now[:notice] = 'Thank you for your message. I will contact you soon!'
 	      redirect_to root_path
+	      flash.now[:notice] = 'Thank you for your message. I will contact you soon!'
 	    else
 	      flash.now[:error] = 'Cannot send message.'
 	      render :new
